4f8b6462e83b5f6807483568157e7ab08b21e26f
Yash Kadam <yashkadam278@gmail.com>
2024-02-04 20:45:06 +0530

    Time: 95 ms (95.34%), Space: 74.2 MB (20.36%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
@@ -1,37 +1,16 @@
 function longestConsecutive(nums: number[]): number {
-  if (nums.length === 0) return 0;
-
-  let curr = nums[0];
-
-  let longestConsecutive = 1;
-
-  let currentConsecutive = 1;
+  let longestConsecutive = 0;
 
   const numSet = new Set(nums);
 
   for (const num of numSet) {
-    curr = num;
-
-    if (numSet.has(curr - 1)) {
-      continue;
-    }
-
-    let next = curr;
-
-    for (let index = 0; index < nums.length; index++) {
-      if (numSet.has(next + 1)) {
-        currentConsecutive += 1;
-        next += 1;
-      } else {
-        if (currentConsecutive > longestConsecutive) {
-          longestConsecutive = currentConsecutive;
-          currentConsecutive = 1;
-          break;
-        } else {
-          currentConsecutive = 1;
-          break;
-        }
+    if (!numSet.has(num - 1)) {
+      let length = 0;
+      while (numSet.has(num + length)) {
+        length += 1;
       }
+
+      longestConsecutive = Math.max(length, longestConsecutive);
     }
   }
 
