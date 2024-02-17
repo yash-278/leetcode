7ba5d881aa82f6f3ce5db9820bb535dadc4ffbc6
Yash Kadam <yashkadam278@gmail.com>
2024-02-17 14:45:16 +0530

    Time: 75 ms (34.40%), Space: 57.3 MB (6.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -4,9 +4,9 @@ function isPalindrome(s: string): boolean {
   for (const letter of s) {
     const code = letter.charCodeAt(0);
     if (
-      !(code > 47 && code < 58) && // numeric (0-9)
-      !(code > 64 && code < 91) && // upper alpha (A-Z)
-      !(code > 96 && code < 123) // lower alpha (a-z)
+      !(code > 47 && code < 58) &&
+      !(code > 64 && code < 91) &&
+      !(code > 96 && code < 123)
     ) {
       continue;
     }
